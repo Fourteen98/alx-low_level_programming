34ee65ee57f478bb63ca7313a56953517d8d7533
Fourteen98 <emuhiydeen6@gmail.com>
2022-04-07 07:23:31 -0700

    added 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,6 +7,6 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
